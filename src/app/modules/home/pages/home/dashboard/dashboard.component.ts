3059e7acfe5bc66cddc84e1313ae01dd350787a6
ArielColqueH <arielcolque444@gmail.com>
2021-04-24 23:02:58 -0400

    categorias importantes cards added

--- src/app/modules/home/pages/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/pages/home/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
+  numero: number = 4;
+  impCategory: ImportantCategory[] = [
+    { title: "Cardiologia", price: 1000, detail: "55% del total" },
+    { title: "Pediatria", price: 800, detail: "55% del total" },
+    { title: "Odontologia", price: 1200, detail: "55% del total" },
+    { title: "Neumologia", price: 500, detail: "55% del total" },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+}
+class ImportantCategory {
+  title: string;
+  price: number;
+  detail: string;
 }
