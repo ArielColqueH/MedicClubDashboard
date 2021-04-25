c1fb2aa5bebcc8f38ef6479108c5f7e7cdfe9a7b
ArielColqueH <arielcolque444@gmail.com>
2021-04-24 21:36:53 -0400

    grafico de atencion por genero

--- src/app/modules/home/pages/home/dashboard/atencion-genero/atencion-genero.component.ts
+++ src/app/modules/home/pages/home/dashboard/atencion-genero/atencion-genero.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { single } from "./data";
 @Component({
-  selector: 'app-atencion-genero',
-  templateUrl: './atencion-genero.component.html',
-  styleUrls: ['./atencion-genero.component.scss']
+  selector: "app-atencion-genero",
+  templateUrl: "./atencion-genero.component.html",
+  styleUrls: ["./atencion-genero.component.scss"],
 })
 export class AtencionGeneroComponent implements OnInit {
+  single: any[];
+  view: any[] = [700, 400];
 
-  constructor() { }
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: string = "below";
 
-  ngOnInit() {
+  colorScheme = {
+    domain: ["#19D3DA", "#B7F9FC"],
+  };
+  ngOnInit() {}
+  constructor() {
+    Object.assign(this, { single });
   }
 
+  onSelect(data): void {
+    console.log("Item clicked", JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log("Activate", JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log("Deactivate", JSON.parse(JSON.stringify(data)));
+  }
 }
---
+++ src/app/modules/home/pages/home/dashboard/atencion-genero/data.js
@@ -0,0 +1,10 @@
+export var single = [
+    {
+      "name": "Masculino",
+      "value": 2000
+    },
+    {
+      "name": "Femenino",
+      "value": 1500
+    },
+  ];
